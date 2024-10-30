2bd4c7f192f3d20e46a8e4f69a18f438925cda45
Lucía Parrilla del Río <luciaparrilla9@hotmail.com>
2024-10-30 16:43:54 +0100

    Terminado 29

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -2,6 +2,10 @@ package com.campusdual.classroom;
 
 public class Exercise29 {
     public static void main(String[] args) {
-
+        try {
+            throw new RuntimeException("Simplest exception ever.");
+        } catch (Exception e) {
+            System.out.println("Simplest exception ever has been handled.");
+        }
     }
 }
